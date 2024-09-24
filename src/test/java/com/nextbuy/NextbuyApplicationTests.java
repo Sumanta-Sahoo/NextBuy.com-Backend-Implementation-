39eba983bbd2592f51e929ae3a3888c1e886b2e7
[Sumanta Sahoo] <sumantasahoo138@gmail.com>
2024-09-24 07:11:42 +0530

    Setting up project folder for backend development

---
+++ src/test/java/com/nextbuy/NextbuyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nextbuy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NextbuyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
